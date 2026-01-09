bc191d5ad1fb65bd525e5d55f846d13fe2a602f4
bwisd91 <bwisd91@gmail.com>
2026-01-09 04:03:30 -0600

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+export const metadata = {
+    title: 'Beau Wisdom – Behavioral & Mental Health Development & Educator',
+    description: 'My personal portfolio and thoughts website',
+};
+
+export default function RootLayout({ children }) {
+    return (
+          <html lang="en">
+                <head>
+                        <meta charSet="UTF-8" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </head>head>
+                <body>{children}</body>body>
+          </html>html>
+        );
+}</html>
