543a2ce1ba9f37c520021a516e4881a178ce9c80
bwisd91 <bwisd91@gmail.com>
2026-01-10 22:54:02 -0600

    Create Header.jsx

---
+++ app/components/Header.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+export default function Header() {
+    return (
+          <header>
+                <div className="header-container">
+                        <h1><a href="/">Beau Wisdom</a>a></h1>h1>
+                        <nav>
+                                  <ul>
+                                              <li><a href="#about">About</a>a></li>li>
+                                              <li><a href="#services">Services</a>a></li>li>
+                                              <li><a href="#thoughts">Thoughts</a>a></li>li>
+                                              <li><a href="#credentials">Credentials</a>a></li>li>
+                                              <li><a href="#contact">Contact</a>a></li>li>
+                                  </ul>ul>
+                        </nav>nav>
+                        <button className="menu-toggle" aria-label="Toggle menu">☰</button>button>
+                </div>div>
+          </header>header>
+        );
+}</header>
